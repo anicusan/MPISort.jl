b88ea2831f4b179dbedd090b89268f0bb572e370
anicusan <a.l.nicusan@bham.ac.uk>
2023-08-19 12:18:24 +0100

    new github workflows

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,12 @@
 using MPISort
 using Documenter
 
-makedocs(modules = [MPISort], sitename = "MPISort.jl")
-
+makedocs(
+    modules = [MPISort],
+    sitename = "MPISort.jl",
+    format = Documenter.HTML(
+        # Only create web pretty-URLs on the CI
+        prettyurls = get(ENV, "CI", nothing) == "true",
+    ),
+)
 deploydocs(repo = "github.com/anicusan/MPISort.jl.git")
